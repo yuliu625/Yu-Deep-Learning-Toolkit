b45c9a472f21a54910c9e5fd69f240c83d86b62d
yu <yul1024@outlook.com>
2025-07-11 20:53:10 +0800

    Update zero_shot_classifier.py

--- natural_language_processing/zero_shot_classification/zero_shot_classifier.py
+++ natural_language_processing/zero_shot_classification/zero_shot_classifier.py
@@ -1,1 +1,15 @@
+"""
+使用transformers具有zero-shot-classification能力的模型。
+
+主要是基于相似度。
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class ZeroShotClassifier:
+    ...
 
