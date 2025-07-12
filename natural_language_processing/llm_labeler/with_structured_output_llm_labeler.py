f28fabb043755477b9101b34f82be92a24e91c3f
yu <yul1024@outlook.com>
2025-07-12 20:06:00 +0800

    Update with_structured_output_llm_labeler.py

--- natural_language_processing/llm_labeler/with_structured_output_llm_labeler.py
+++ natural_language_processing/llm_labeler/with_structured_output_llm_labeler.py
@@ -4,10 +4,10 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 if TYPE_CHECKING:
     from langchain_core.language_models import BaseChatModel
-    from langchain_core.messages import SystemMessage
+    from langchain_core.messages import SystemMessage, HumanMessage
     from pydantic import BaseModel
 
 
@@ -21,7 +21,39 @@ class SimpleLabeler:
         schema_pydantic_base_model: type[BaseModel],
         system_message: SystemMessage,
     ):
-        self._llm = llm
-        self._schema_pydantic_base_model = schema_pydantic_base_model
+        self._structured_llm = self._get_structured_llm(
+            llm=llm,
+            schema_pydantic_base_model=schema_pydantic_base_model,
+        )
         self._system_message = system_message
+
+    async def label_data(
+        self,
+        data: str,
+    ) -> BaseModel:
+        structured_data = await self._call_llm(
+            human_message=HumanMessage(content=data),
+        )
+        return structured_data
+
+    async def _call_llm(
+        self,
+        human_message: HumanMessage,
+    ) -> BaseModel:
+        response = await self._structured_llm.ainvoke(input=[
+            self._system_message,
+            human_message,
+        ])
+        return response
+
+    def _get_structured_llm(
+        self,
+        llm: BaseChatModel,
+        schema_pydantic_base_model: type[BaseModel],
+    ) -> BaseChatModel:
+        structured_llm = llm.with_structured_output(
+            schema=schema_pydantic_base_model,
+        )
+        structured_llm = cast('BaseChatModel', structured_llm)
+        return structured_llm
 
