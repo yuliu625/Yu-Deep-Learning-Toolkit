ba0f5ee739bf603721dffd9e9b7edb4492b3b693
yu <yul1024@outlook.com>
2026-02-10 17:43:56 +0800

    to: add dashscope readers

--- natural_language_processing/llm_reader/dashscope_reader.py
+++ natural_language_processing/llm_reader/dashscope_reader.py
@@ -17,10 +17,40 @@ Notes:
 from __future__ import annotations
 from loguru import logger
 
+from openai import OpenAI
+
+import os
+from pathlib import Path
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
 class QwenLongReader:
-    ...
+    @staticmethod
+    def read_file(
+        file_id: str,
+        system_message_str: str,
+        human_message_str: str,
+        result_path: str,
+    ):
+        # 构造client，使用OpenAI Client兼容方法。
+        client = OpenAI(
+            # HARDCODED
+            base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+            api_key=os.getenv("DASHSCOPE_API_KEY"),
+        )
+        completion = client.chat.completions.create(
+            # HARDCODED
+            model="qwen-long",
+            messages=[
+                # sys1: 角色定义
+                {'role': 'system', 'content': 'You are a helpful assistant.'},
+                # sys2: 文档内容（纯文本或file-id）
+                {'role': 'system', 'content': f'fileid://{file_id}'},
+                #
+                {'role': 'user', 'content': '这篇文章讲了什么?'}
+            ],
+        )
+        logger.info(f"completion: \n{completion}")
 
